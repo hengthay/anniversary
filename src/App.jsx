694694ec19914e53e99297d6b68b6fe2f4d05254
hengthay <laovkimhengthai@gmail.com>
2025-11-07 13:05:30 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+// App.jsx / App.tsx
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Box from './pages/Box'
-import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom'
 import Card from './components/Card'
 import Main from './components/Main'
 import Video from './components/Video'
@@ -8,13 +9,15 @@ import Message from './components/Message'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path="/" element={<Box />} index/>
-        <Route path='/card' element={<Card />}/>
-        <Route path='/main' element={<Main />}/>
-        <Route path='/video' element={<Video />}/>
-        <Route path='/message' element={<Message />}/>
+        <Route path="/" element={<Box />} />            {/* home */}
+        <Route path="card" element={<Card />} />
+        <Route path="main" element={<Main />} />
+        <Route path="video" element={<Video />} />
+        <Route path="message" element={<Message />} />
+        {/* optional catch-all */}
+        {/* <Route path="*" element={<Box />} /> */}
       </Routes>
     </BrowserRouter>
   )
